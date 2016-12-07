904ceda7d1acf43a3535685ef04345c6751dae28
a757596945 <757596945@qq.com>
2016-12-07 09:55:11 +0800

    testMavenWeb

---
+++ testMavenWeb/src/main/java/com/sun/testMavenWeb/testMavenWeb/App.java
@@ -0,0 +1,13 @@
+package com.sun.testMavenWeb.testMavenWeb;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
